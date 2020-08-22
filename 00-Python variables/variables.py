840dac6c43d996cd6c54e19b401473a3774c22b5
pythontoday <pythontodaynet@gmail.com>
2020-08-22 16:17:26 +0700

    Initial commit

---
+++ 00-Python variables/variables.py
@@ -0,0 +1,49 @@
+name = 'Alex'
+age = 29
+family = ['Anna', 'Victor', 'Alex', 'Jhon']
+
+# print(f'Тип name: {type(name)}')
+# print(f'Тип age: {type(age)}')
+# print(f'Тип family: {type(family)}')
+#
+# print("#" * 20)
+#
+# print(f'ID name: {id(name)}')
+# print(f'ID age: {id(age)}')
+# print(f'ID family: {id(family)}')
+
+# underscore_notation = 999
+# camelCase = 'Hello World'
+#
+# # good practice
+# my_name = 'Pavel'
+# auntName = 'Anna'
+
+# bad practice
+# 2pac = 'Old School'
+# $money = 'power'
+# _age = 22
+
+# import keyword
+# kw_list = keyword.kwlist
+# print(kw_list)
+#
+# for kw in kw_list:
+#       print(kw)
+
+# return = 222
+
+# x = 14
+# X = 'Python'
+# print(x)
+# print(X)
+
+# # good
+# name
+# # bad
+# n
+#
+# # good
+# user_address
+# # bad
+# personUserHomeAddress
