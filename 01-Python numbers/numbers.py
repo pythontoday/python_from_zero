9058a3e830c1307e7fa80b75935d93421ce38a61
pythontoday <pythontodaynet@gmail.com>
2020-09-05 16:29:16 +0700

    add 01-Python numbers

---
+++ 01-Python numbers/numbers.py
@@ -0,0 +1,69 @@
+# x = 10
+# print(type(x))
+#
+# y = 14.99
+# print(type(y))
+
+# x = 5
+# print(id(x))
+#
+# x = 6
+# print(id(x))
+
+# x = 22 // 7
+# print(x)
+
+# x = 7 % 2
+# print(x)
+
+# def my_func(num):
+#
+#     if num % 2 == 0:
+#         print(f"Число {num} чётное!")
+#     else:
+#         print(f"Число {num} нечётное!")
+#
+# my_func(11)
+
+# x = 3 ** 3
+# print(x)
+
+# x = 14 / 2
+# print(x)
+# print(type(x))
+#
+# x = int(x)
+# print(x)
+# print(type(x))
+
+# x = 21
+# print(x)
+# print(type(x))
+#
+# x = float(x)
+# print(x)
+# print(type(x))
+
+# x = 7.6
+# print(int(x))
+
+# x = int('1') + 2
+# print(x)
+
+# x = round(4.555)
+# print(x)
+#
+# y = round(3.7825, 2)
+# print(y)
+
+# x = min(16, 9, -3, 42, 25)
+# print(x)
+#
+# y = max(90, 4, -2, 54, 63)
+# print(y)
+
+# x = abs(-13.44)
+# print(x)
+
+# x = min(7, 44, 9, abs(-2), 13, 78)
+# print(x)
