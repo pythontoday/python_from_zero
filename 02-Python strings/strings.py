926222139864e004756764f656d57a2486a3d7ae
pythontoday <pythontodaynet@gmail.com>
2020-09-14 17:17:16 +0700

    add 02-Python strings

---
+++ 02-Python strings/strings.py
@@ -0,0 +1,140 @@
+# my_string_1 = 'I like Python'
+# my_string_2 = "I like Python"
+# my_string_3 = """I like Python"""
+
+# my_string_4 = 'He likes Python"
+# print(my_string_4)
+
+# my_string_1 = "I 'like' Python"
+# print(my_string_1)
+
+# my_string_1 = 'I "like" Python'
+# print(my_string_1)
+
+# my_string_1 = "I \"like\" Python"
+# print(my_string_1)
+
+# my_string = """Python
+# "is"
+# awesome
+# """
+# print(my_string)
+
+# my_string = "Hello \nworld"
+# print(my_string)
+
+# my_string = "Python"
+# print(id(my_string))
+# my_string = "JavaScript"
+# print(id(my_string))
+
+# my_string = "Python " + "is" + " awesome!"
+# print(my_string)
+#
+# my_string = "Go" * 10
+# print(my_string)
+
+# my_string = "Python"
+# print(len(my_string))
+
+# my_string = ""
+# print(len(my_string))
+# my_string = " "
+# print(len(my_string))
+
+# my_string = "Python"
+# print(my_string)
+# print(type(my_string))
+# print(id(my_string))
+
+# my_string = "python"
+# for mth in dir(my_string):
+#     print(mth)
+
+# my_string = my_string.upper()
+# print(my_string)
+# my_string = my_string.lower()
+# print(my_string)
+# my_string = my_string.title()
+# print(my_string)
+
+# my_string = "I like PHP"
+# print(my_string)
+# my_string = my_string.replace("PHP", "Python")
+# print(my_string)
+
+# my_string = "I like Python"
+# my_string = my_string.split(" ")
+# print(my_string)
+
+# my_string = "I " + "like" + " Python"
+# print(my_string)
+
+# my_string = "-".join(my_string)
+# print(my_string)
+
+# my_string = "I like Python!          "
+# my_string = my_string.strip()
+# print(my_string)
+#
+# my_string = my_string.strip("!")
+# print(my_string)
+
+# my_string = "@I like Python!"
+# print(my_string)
+# my_string = my_string.lstrip("@")
+# print(my_string)
+# my_string = my_string.rstrip("!")
+# print(my_string)
+
+# name = "Андрей"
+# my_string = f"Привет {name}!"
+# print(my_string)
+#
+# name_1 = "Кристина"
+# name_2 = "Влад"
+# my_string = f"Привет {name}, {name_1}, {name_2}!"
+# print(my_string)
+
+# name = "Анна"
+# my_string = "Привет {}".format(name)
+# print(my_string)
+
+# name = "Анна"
+# my_string = "Привет {name}{important}".format(name=name, important="!")
+# print(my_string)
+
+# name = "Алекс"
+# my_string = "Привет %s"
+# print(my_string % name)
+
+my_string = "Python is awesome"
+# print(my_string)
+# print(my_string[0])
+# print(my_string[1])
+# print(my_string[2])
+# print(my_string[3])
+# print(my_string[4])
+# print(my_string[5])
+# print(my_string[6])
+
+# print(len(my_string))
+# print(my_string[17])
+# print(my_string[len(my_string) - 1])
+# print(my_string[-1])
+# print(my_string[-2])
+# print(my_string[-3])
+# print(my_string[-4])
+# print(my_string[-5])
+# print(my_string[-6])
+
+# my_string = my_string[0:6]
+# print(my_string)
+# my_string = my_string[7:9]
+# print(my_string)
+# print(my_string[:])
+
+# my_string = "123456789"
+# print(my_string)
+# print(my_string[::2])
+# print(my_string[::-1])
