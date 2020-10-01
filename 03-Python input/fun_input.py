d1dac687df08b30083a2a51eead15e19b68f4e13
pythontoday <pythontodaynet@gmail.com>
2020-10-01 13:39:31 +0700

    add 03-Python input

---
+++ 03-Python input/fun_input.py
@@ -0,0 +1,48 @@
+# name = input("Введите ваше имя: ")
+# print(name)
+
+# birth_date = input("Введите дату рождения в формате дд.мм.гг(пример: 01.01.1970): ")
+# print(birth_date)
+
+# my_var = input("Введите данные: ")
+# print(type(my_var))
+
+# name = input("Введите ваше имя: ")
+# print(name)
+# print(type(name))
+#
+# age = input("Введите ваш возраст: ")
+# print(age)
+# print(type(age))
+# age = age * 3
+# print(age)
+
+# age = input("Введите ваш возраст: ")
+# print(age)
+# print(type(age))
+#
+# age = int(age)
+# print(type(age))
+#
+# age = age * 3
+# print(age)
+
+# age = int(input("Введите ваш возраст: "))
+# age = age / 2
+# print(age)
+
+# amount = float(input("Процент по вкладу составит: "))
+# print(amount)
+
+# shape_len = float(input("Введите длину прямоугольника: "))
+# shape_width = float(input("Введите ширину прямоугольника: "))
+# shape_area = shape_len * shape_width
+#
+# print(f"Площадь прямоугольника равна: {shape_area}")
+
+# your_age = int(input("Сколько вам лет? - "))
+#
+# if your_age < 18:
+#     print("Подрасти сынок")
+# else:
+#     print("Добро пожаловать на наш ХАБ :))")
